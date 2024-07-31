3f3cac2ff20e121a16adb7042aafda06783e577a
Panfilova Maria <reg@panfilova.com>
2024-07-31 13:02:33 +0300

    square rework

--- app/src/main/java/com/example/myapplication/SnakePiece.kt
+++ app/src/main/java/com/example/myapplication/SnakePiece.kt
@@ -4,13 +4,4 @@ import android.graphics.Canvas
 import android.graphics.Paint
 
 class SnakePiece(): Square() {
-    override fun draw(canvas: Canvas, size: Int, offsetX: Int, offsetY: Int) {
-        canvas.drawRect(
-            x.toFloat()*size + offsetX,
-            y.toFloat()*size + offsetY,
-            (x + 1)*size.toFloat() + offsetX,
-            (y + 1)*size.toFloat() + offsetY,
-            Paint()
-        )
-    }
 }
--- app/src/main/java/com/example/myapplication/Square.kt
+++ app/src/main/java/com/example/myapplication/Square.kt
@@ -1,12 +1,23 @@
 package com.example.myapplication
 
 import android.graphics.Canvas
+import android.graphics.Paint
 
 abstract class Square: Comparable<Square> {
+    protected val paint: Paint = Paint()
+
     var x: Int = 0
     var y: Int = 0
 
-    abstract fun draw(canvas: Canvas, size: Int, offsetX: Int = 0, offsetY: Int = 0)
+    fun draw(canvas: Canvas, size: Int, offsetX: Int = 0, offsetY: Int = 0) {
+        canvas.drawRect(
+            x.toFloat()*size + offsetX,
+            y.toFloat()*size + offsetY,
+            (x + 1)*size.toFloat() + offsetX,
+            (y + 1)*size.toFloat() + offsetY,
+            paint
+        )
+    }
 
     override fun compareTo(other: Square): Int {
         return if (x == other.x && y == other.y) return 0 else return 1
